e982c36504e0945afbbe3e6d96f3f76071210c5e
Prizrak <60360057+Prizrak11@users.noreply.github.com>
2020-07-11 16:01:06 -0500

    full start

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+//Sass
+require('../sass/main.scss')
+//Images
+require('../static/alba-logo.png')
+//Babel
+import "core-js/stable";
+import "regenerator-runtime/runtime";
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports = {
+    entry:'./src/js/main.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use:[
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-object-rest-spread'],
+                    }
+                }
+            },
+            {
+                test: /\.pug$/,
+                loader:[
+                    'pug-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'static/',
+                            useRelativePath: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/pages/index.pug'
+        }),
+        new MiniCssExtractPlugin({
+            filename:'style.css'
+        })
+    ]
+}
